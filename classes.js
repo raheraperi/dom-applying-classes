d483b4fc06deaf2cb32c234a59736562937842f0
Rachael Peri <raheraperi@Rachaels-Air.NetComm.Wireless>
2018-04-14 14:57:37 +1200

    chnanges to dom-applying-classes

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()  
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -19,7 +21,21 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
-
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
-
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+  // Next, we apply a new CSS class to it:
+  three.classList.add('orange')
+}
 // CREATE FUNCTION makeVisible HERE
+  function makeVisible () {
+    var makeVisible = document.getElementsByClassName('invisible')[0]
+    makeVisible.classList.add('visible')
+}
